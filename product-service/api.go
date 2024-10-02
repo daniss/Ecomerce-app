8cf119564302ab750fff79645d821f012419b62e
daniss <daniscin@gmail.com>
2024-10-02 21:09:38 +0200

    [ADD] start set up of the api

---
+++ product-service/api.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"fmt"
+)
+
+func product(r *gin.Engine) {
+	r.GET("/products", func(c *gin.Context) {
+
+	})
+
+	r.GET("/products/:id", func(c *gin.Context) {
+		
+	})
+
+	r.POST("/products", func(c *gin.Context) {
+
+	})
+
+	r.PUT("/products/:id", func(c *gin.Context) {
+
+	})
+
+	r.DELETE("/products/:id", func(c *gin.Context) {
+		
+	})
+}
+
+func 
